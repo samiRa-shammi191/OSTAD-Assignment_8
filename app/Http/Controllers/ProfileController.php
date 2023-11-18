fabfda306a7ac5e147544857f47f38d3451c5dae
Samira Jahan <samirashammi67@gmail.com>
2023-11-19 01:14:00 +0600

    cookie commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Cookie;
+
 
 class ProfileController extends Controller
 {
-    //
+    public function index($id)
+    {
+        
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            "id" => $id,
+            "name" => $name,
+            "age" => $age
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+      
+
+        $response = new Response(json_encode($data));
+        $cookie = Cookie::make($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+        $response->withCookie($cookie);
+
+return $response;
+    }
 }
